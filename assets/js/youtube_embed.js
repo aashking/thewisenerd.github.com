0c523363255455104af5f19706936a4951eb7946
Vineeth Raj <contact.twn@openmailbox.org>
2014-10-17 23:21:11 +0530

    blog update on 17 Oct 2014 at 23:21:11 IST

--- assets/js/youtube_embed.js
+++ assets/js/youtube_embed.js
@@ -39,43 +39,8 @@ function fix_player(embed_id) {
 	return embed_code;
 }
 
-
-//aefxx: http://stackoverflow.com/a/5830517/2873157
-function css(a) {
-    var sheets = document.styleSheets, o = {};
-    for (var i in sheets) {
-        var rules = sheets[i].rules || sheets[i].cssRules;
-        for (var r in rules) {
-            if (a.is(rules[r].selectorText)) {
-                o = $.extend(o, css2json(rules[r].style), css2json(a.attr('style')));
-            }
-        }
-    }
-    return o;
-}
-
-function css2json(css) {
-    var s = {};
-    if (!css) return s;
-    if (css instanceof CSSStyleDeclaration) {
-        for (var i in css) {
-            if ((css[i]).toLowerCase) {
-                s[(css[i]).toLowerCase()] = (css[css[i]]);
-            }
-        }
-    } else if (typeof css == "string") {
-        css = css.split("; ");
-        for (var i in css) {
-            var l = css[i].split(": ");
-            s[l[0].toLowerCase()] = (l[1]);
-        }
-    }
-    return s;
-}
-
 $(window).bind("load", function() {
-	$('.embed-youtube').on('click', function(e) {
-		alert("test");
+	$('#ajax-container').on('click', '.embed-youtube', function(e) {
 
 		$(".embed-youtube").attr("class", "embed-youtube-vid");
 
