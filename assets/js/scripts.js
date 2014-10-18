266a7d544ef55825f278887c7710e8eb4327b613
Vineeth Raj <contact.twn@openmailbox.org>
2014-10-18 21:59:08 +0530

    blog revamp
    bye bye .html links! :D

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,17 +28,22 @@ jQuery(function($) {
     // Show the index if the url has "page" in it (a simple
     // way of checking if we're on a paginated page.)
     if (window.location.pathname.indexOf('page') === 1) {
+		console.log("window.location.pathname.indexOf('page') == 1");
         $latestPost.hide();
         $postIndex.show();
     }
 
     // Check if history is enabled for the browser
     if ( ! History.enabled) {
+		console.log("! History.enabled");
         return false;
     }
 
     History.Adapter.bind(window, 'statechange', function() {
         var State = History.getState();
+        console.log ("History.getState()");
+        console.log( State );
+        console.log("----------------");
 
         // Get the requested url and replace the current content
         // with the loaded content
@@ -51,16 +56,20 @@ jQuery(function($) {
             $ajaxContainer.fadeOut(500, function() {
                 $latestPost = $newContent.filter('#latest-post');
                 $postIndex = $newContent.filter('#post-index');
-
-                if (showIndex === true) {
-                    $latestPost.hide();
-                } else {
-                    $latestPost.show();
-                    $postIndex.hide();
-                }
+				
+				console.log( "showIndex == "+ showIndex );
+				console.log("----------------");
 
                 $ajaxContainer.html($newContent);
                 $ajaxContainer.fadeIn(500);
+
+                if (showIndex === true) {
+                    $('#latest-post').hide().fadeOut();
+                    $('#post-index').fadeIn();
+                } else {
+                    $('#post-index').hide().fadeOut();
+                    $('#latest-post').fadeIn();
+                }
 
                 NProgress.done();
 
@@ -72,22 +81,30 @@ jQuery(function($) {
 
     $('body').on('click', '.js-ajax-link, .pagination a', function(e) {
         e.preventDefault();
-
+		console.log ("body onclick loading"  + loading );
         if (loading === false) {
             var currentState = History.getState();
+            console.log("currentState");
+            console.log(currentState);
             var url = $(this).attr('href');
+            console.log("url:" + url);
             var title = $(this).attr('title') || null;
+            console.log("title:" + title);
 
             // If the requested url is not the current states url push
             // the new state and make the ajax call.
             if (url !== currentState.url.replace(/\/$/, "")) {
+				console.log(url + "!==" + currentState.url.replace(/\/$/, "") );
                 loading = true;
 
                 // Check if we need to show the post index after we've
                 // loaded the new content
                 if ($(this).hasClass('js-show-index') || $(this).parent('.pagination').length > 0) {
                     showIndex = true;
-                }
+                } else {
+					showIndex = false;
+				}
+				console.log ("showIndex" + showIndex);
 
                 NProgress.start();
 
@@ -96,22 +113,26 @@ jQuery(function($) {
                 // Swap in the latest post or post index as needed
                 if ($(this).hasClass('js-show-index')) {
                     $('html, body').animate({'scrollTop': 0});
+                    console.log ("case 2");
 
                     NProgress.start();
 
-                    $latestPost.fadeOut(300, function() {
-                        $postIndex.fadeIn(300);
-                        NProgress.done();
-                    });
+                    $('#latest-post').hide().fadeOut();
+                    $('#post-index').fadeIn();
+                    NProgress.done();
+
+                    showIndex = true;
                 } else {
                     $('html, body').animate({'scrollTop': 0});
+                    console.log ("case 3");
 
                     NProgress.start();
 
-                    $postIndex.fadeOut(300, function() {
-                        $latestPost.fadeIn(300);
-                        NProgress.done();
-                    });
+                    $('#post-index').hide().fadeOut();
+                    $('#latest-post').fadeIn();
+                    NProgress.done();
+                    showIndex = false;
+
                 }
             }
         }
